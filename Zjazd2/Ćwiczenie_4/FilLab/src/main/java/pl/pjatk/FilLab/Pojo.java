798ff3f2d67311171534a54865373f8a24ef8639
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-20 22:21:21 +0200

    Add files via upload

---
+++ Zjazd2/Ćwiczenie_4/FilLab/src/main/java/pl/pjatk/FilLab/Pojo.java
@@ -0,0 +1,28 @@
+package pl.pjatk.FilLab;
+
+public class Pojo {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Pojo(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Pojo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
