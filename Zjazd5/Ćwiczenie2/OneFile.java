77b218c16a9c16e14ba150c42dc2835df0287440
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-22 00:09:13 +0200

    Create OneFile.java

---
+++ Zjazd5/Ćwiczenie2/OneFile.java
@@ -0,0 +1,46 @@
+//Klasa Movie po zmianie, aby połączyć się z bazą danych
+
+package pl.pjatk.MovieService.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Strategia Identity, która wykorzystuje autoinkrementację bazy danych do generowania unikalnych wartości.
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "category")
+    private String category;
+    // Konstruktory, gettery i settery
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Dodaj gettery i settery dla innych pól
+}
