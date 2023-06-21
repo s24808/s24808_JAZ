88d529349b347585c3f3c825bdb7d25d321609a6
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-21 22:29:26 +0200

    Add files via upload

---
+++ Zjazd4/Ćwiczenie_3e/MovieService/src/main/java/pl/pjatk/MovieService/service/MovieService.java
@@ -0,0 +1,51 @@
+package pl.pjatk.MovieService.service;
+
+import org.springframework.stereotype.Service;
+import pl.pjatk.MovieService.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    private List<Movie> movies;
+    private int nextId;
+    public MovieService() {
+        movies = new ArrayList<>();
+        movies.add(new Movie(1, "Film 1", "Kategoria 1"));
+        movies.add(new Movie(2, "Film 2", "Kategoria 2"));
+        movies.add(new Movie(3, "Film 3", "Kategoria 3"));
+    }
+
+    public List<Movie> getAllMovies() {
+        return movies;
+    }
+
+    public Movie getMovieById(int id) {
+        for (Movie movie : movies) {
+            if (movie.getId() == (id)) {
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    public Movie addMovie(Movie movie) {
+        movie.setId(nextId++);
+        movies.add(movie);
+        return movie;
+    }
+
+    public void updateMovie(Movie updatedMovie) {
+        for (int i = 0; i < movies.size(); i++) {
+            Movie movie = movies.get(i);
+            if (movie.getId() == updatedMovie.getId()) {
+                movies.set(i, updatedMovie);
+                break;
+            }
+        }
+    }
+
+    // Pozostałe metody serwisowe
+}
