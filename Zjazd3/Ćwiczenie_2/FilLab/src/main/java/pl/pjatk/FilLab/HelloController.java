163b1b825228cd628264f1fc55f92ddc5474d9a7
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-21 19:51:08 +0200

    Add files via upload

---
+++ Zjazd3/Ćwiczenie_2/FilLab/src/main/java/pl/pjatk/FilLab/HelloController.java
@@ -0,0 +1,20 @@
+package pl.pjatk.FilLab;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello world");
+    }
+
+    @GetMapping("/model")
+    public ResponseEntity<Car> getModel() {
+        Car car = new Car("Brand", "Model", 2023);
+        return ResponseEntity.ok(car);
+    }
+}
