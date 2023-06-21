b79c5dea455da2ab4e03cacd91f0e8d4301ba737
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-21 20:14:39 +0200

    Create OneFile.java

---
+++ Zjazd3/Ćwiczenia_5/OneFile.java
@@ -0,0 +1,91 @@
+//Dependencje z ćwiczenia 1
+
+<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-web</artifactId>
+		</dependency>
+  
+//Klasa Car
+
+package pl.pjatk.FilLab;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    // Konstruktory, gettery, settery
+
+    // Przykładowe implementacje konstruktora i getterów/setterów
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
+//Klasa Hello Controller, poprzednie mapowanie /hello, musi być zakomentarzowane ze względu na powielające się zapytanie
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+    /*@GetMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello world");
+    }*/
+
+    /*@GetMapping("/model")
+    public ResponseEntity<Car> getModel() {
+        Car car = new Car("Brand", "Model", 2023);
+        return ResponseEntity.ok(car);
+    }*/
+
+    @GetMapping("/hello/{someValue}")
+    public ResponseEntity<String> helloValue(@PathVariable String someValue) {
+        return ResponseEntity.ok(someValue);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloRequestParam(@RequestParam("reqParam") String someValue) {
+        return ResponseEntity.ok(someValue);
+    }
+
+    @PostMapping("/model")
+    public ResponseEntity<Car> createCar(@RequestBody Car car) {
+        // Przetwarzanie danych dostarczonych w body
+        // Tutaj możesz wykonać logikę tworzenia nowego obiektu na podstawie dostarczonych danych
+
+        // Przykładowa implementacja
+        Car newCar = new Car(car.getBrand(), car.getModel(), car.getYear());
+
+        return ResponseEntity.ok(newCar);
+    }
+}
