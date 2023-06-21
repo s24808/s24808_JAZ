755f3a8cd9eb49e5d6743ed81afb6b93eb4a9026
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-22 00:54:48 +0200

    Add files via upload

---
+++ Zjazd5/ZadanieDomowe/MovieService/src/main/java/pl/pjatk/MovieService/model/Movie.java
@@ -0,0 +1,54 @@
+package pl.pjatk.MovieService.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private boolean isAvailable;
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
