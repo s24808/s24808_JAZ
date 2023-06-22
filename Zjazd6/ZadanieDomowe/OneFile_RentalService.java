203c3c960db66bad5ef242bf605b82b7ead6a422
s24808 <93827195+s24808@users.noreply.github.com>
2023-06-23 00:19:21 +0200

    Update OneFile_RentalService.java

--- Zjazd6/ZadanieDomowe/OneFile_RentalService.java
+++ Zjazd6/ZadanieDomowe/OneFile_RentalService.java
@@ -70,9 +70,11 @@ public class Movie {
 package pl.pjatk.RentalService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import pl.pjatk.RentalService.model.Movie;
 import pl.pjatk.RentalService.service.RentalService;
 
@@ -95,16 +97,21 @@ public class RentalController {
         return restTemplate.getForObject(movieServiceUrl, Movie.class);
     }
 
-    @PutMapping("/movies/{id}/return")
-    public Movie returnMovie(@PathVariable int id) {
-        String movieServiceUrl = "http://localhost:8080/movies/" + id + "/availability";
-        return restTemplate.exchange(movieServiceUrl, HttpMethod.PUT, null, Movie.class).getBody();
-    }
-
     @PutMapping("/movies/{id}/rent")
     public Movie rentMovie(@PathVariable int id) {
         String movieServiceUrl = "http://localhost:8080/movies/" + id + "/availability";
-        return restTemplate.exchange(movieServiceUrl, HttpMethod.PUT, null, Movie.class).getBody();
+
+        try {
+            return restTemplate.exchange(movieServiceUrl, HttpMethod.PUT, null, Movie.class).getBody();
+        } catch (HttpClientErrorException.NotFound ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (HttpClientErrorException.BadRequest ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        } catch (HttpServerErrorException.InternalServerError ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_GATEWAY);
+        } catch (RestClientException ex) {
+            throw new ResponseStatusException(HttpStatus.GATEWAY_TIMEOUT);
+        }
     }
 }
 
