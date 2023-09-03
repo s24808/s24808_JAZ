8394be6c2f1783334fddb555378ab3745944bf5a
s24808 <93827195+s24808@users.noreply.github.com>
2023-09-03 15:01:27 +0200

    Add files via upload

---
+++ Projekt/ŚredniKursWalutyOdDo(DATY)/Zad1/nbp/src/main/java/pl/pjatk/nbp/NbpResponse.java
@@ -0,0 +1,15 @@
+package pl.pjatk.nbp;
+
+import java.util.List;
+
+public class NbpResponse {
+    private List<Rate> rates;
+
+    public List<Rate> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
+}
---
+++ Projekt/ŚredniKursWalutyOdDo(DATY)/Zad1/nbp/src/main/java/pl/pjatk/nbp/service/CurrencyService.java
@@ -0,0 +1,54 @@
+package pl.pjatk.nbp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.pjatk.nbp.NbpResponse;
+import pl.pjatk.nbp.Rate;
+import pl.pjatk.nbp.model.CurrencyQuery;
+import pl.pjatk.nbp.model.CurrencyType;
+import pl.pjatk.nbp.repository.CurrencyQueryRepository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Service
+public class CurrencyService {
+    private final CurrencyQueryRepository currencyQueryRepository;
+    private final RestTemplate restTemplate;
+
+    @Autowired
+
+    public CurrencyService(CurrencyQueryRepository currencyQueryRepository, RestTemplate restTemplate) {
+        this.currencyQueryRepository = currencyQueryRepository;
+        this.restTemplate = restTemplate;
+    }
+
+    public double calculateAverageExchangeRate(CurrencyType currency, LocalDate startDate, LocalDate endDate) {
+        // Pobierz dane z API NBP dla danej waluty i liczby dni
+        String apiUrl = "http://api.nbp.pl/api/exchangerates/rates/A/{currency}/{startDate}/{endDate}/?format=json";
+        String url = apiUrl.replace("{currency}", currency.name())
+                .replace("{startDate}", startDate.toString())
+                .replace("{endDate}", endDate.toString());
+        NbpResponse nbpResponse = restTemplate.getForObject(url, NbpResponse.class);
+
+        // Oblicz średni kurs z pobranych danych
+        List<Rate> rates = nbpResponse.getRates();
+        double sum = rates.stream().mapToDouble(Rate::getMid).sum();
+        double averageRate = sum / rates.size();
+
+        // Zapisz zapytanie do bazy danych
+        CurrencyQuery currencyQuery = new CurrencyQuery();
+        currencyQuery.setCurrency(currency);
+        currencyQuery.setStartDate(startDate);
+        currencyQuery.setEndDate(endDate);
+        currencyQuery.setCalculatedRate(averageRate);
+        currencyQuery.setQueryDate(LocalDate.now());
+        currencyQuery.setQueryTime(LocalTime.now());
+
+        currencyQueryRepository.save(currencyQuery);
+
+        return averageRate;
+    }
+}
