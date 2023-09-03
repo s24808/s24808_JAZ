debb9a527c54854e1bd6f81bf63f9283bb3165b6
s24808 <93827195+s24808@users.noreply.github.com>
2023-09-03 14:53:44 +0200

    Delete Projekt/ŚredniKursWalutyOdDo(DATY)/table.sql

--- Projekt/ŚredniKursWalutyOdDo(DATY)/table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE currency_query (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    currency TINYINT NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    calculated_rate DOUBLE NOT NULL,
-    query_date DATE NOT NULL,
-    query_time TIME NOT NULL
-);
